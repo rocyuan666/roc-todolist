3a597efe042794353998fd41b200678ffc7e8e30
袁鹏 <rocyuan666@163.com>
2022-11-14 20:17:32 +0800

    db

--- db/roc_todolist.sql
+++ db/roc_todolist.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50726
  File Encoding         : 65001
 
- Date: 11/11/2022 17:16:34
+ Date: 14/11/2022 20:17:08
 */
 
 SET NAMES utf8mb4;
@@ -25,13 +25,13 @@ CREATE TABLE `roc_matter`  (
   `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'id',
   `userId` bigint(20) NOT NULL COMMENT '用户id',
   `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '事项名称',
-  `level` int(255) NOT NULL DEFAULT 1 COMMENT '事项优先级分为：1234 默认1',
+  `level` int(255) NOT NULL DEFAULT 0 COMMENT '事项优先级分为：0123 默认0',
   `status` int(255) NOT NULL DEFAULT 0 COMMENT '0待办  1已办 默认0',
   `addtime` datetime(0) NULL DEFAULT NULL COMMENT '添加时间',
   `edittime` datetime(0) NULL DEFAULT NULL COMMENT '修改时间',
   `deltime` datetime(0) NULL DEFAULT NULL COMMENT '删除时间',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = MyISAM AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = MyISAM AUTO_INCREMENT = 7 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for roc_user
@@ -50,7 +50,5 @@ CREATE TABLE `roc_user`  (
 -- Records of roc_user
 -- ----------------------------
 INSERT INTO `roc_user` VALUES (4, 'rocyuan', '284C3A05AC2CC4898AB9EC716D58D3B6', '2022-11-11 13:58:39', NULL);
-INSERT INTO `roc_user` VALUES (5, 'rocyuan666', 'DFF8251F658A29007647B3FCFF85224C', '2022-11-11 14:00:31', NULL);
-INSERT INTO `roc_user` VALUES (6, 'yuanpeng', 'C7A1E04F27DDEFA048A4B8295BAF160C', '2022-11-11 16:46:13', NULL);
 
 SET FOREIGN_KEY_CHECKS = 1;
