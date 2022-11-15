31ae43b22c9f12abc968ce88015ae52998208faf
袁鹏 <rocyuan666@163.com>
2022-11-15 14:56:04 +0800

    config

--- web-ui-react/src/common/config.ts
+++ web-ui-react/src/common/config.ts
@@ -8,7 +8,7 @@ const config: Iconfig = {
   // 网络请求开发环境地址
   devBaseUrl: "http://localhost:18080",
   // 网络请求生产环境地址
-  proBaseUrl: "http://localhost:18080",
+  proBaseUrl: "http://todolist.rocyuan.top:8080",
   // 网络请求超时时长设置（单位：s）
   timeOut: 60,
 };
