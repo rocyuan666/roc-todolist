5bfd28d1cc92c5fab34cd02c070c0c0354e8688b
rocyuan <rocyuan666@163.com>
2022-11-27 21:56:40 +0800

    redis 过期时间

--- web-koa/src/controller/captcha.controller.js
+++ web-koa/src/controller/captcha.controller.js
@@ -6,7 +6,10 @@ class CaptchaController {
   async getCaptcha(ctx, next) {
     const cap = parseInt(Math.random() * 9000 + 1000);
     const uuid = createUUID();
-    await redisClient.set(uuid, cap, 60 * 60);
+    await redisClient.set(uuid, cap, {
+      EX: 60, // 过期时间60秒(1分钟)
+      NX: true, // 键不存在时，对键进行设置
+    });
     ctx.body = await captchaService.getCaptcha(cap, uuid);
   }
 }
